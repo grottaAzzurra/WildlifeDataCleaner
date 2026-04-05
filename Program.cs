b07b04496eeb7c818364e3942575216df82c713c
perspicacious <125438751+grottaAzzurra@users.noreply.github.com>
2026-04-05 12:34:35 -0700

    print a set of animals

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("This program will clean messy data.");
+﻿string[] animals = {"Peregrine Falcon","Polar Bear","Black Panther","Sea Otter","Monarch Butterfly","White Wolf","Red Dragon","Golden Retriever","Spanish Bull"};
+
+for (int i=0; i < animals.Length; i++)
+{
+    Console.WriteLine(animals[i]);
+}
